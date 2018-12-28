ff5318b474ead402e16bd7242d4832b093b3a073
Takechanyuki <yuki6023778@gmail.com>
2018-12-28 18:11:18 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,10 @@
 <?php
 
-require_once__DIR__ . '/vendor/autoload.php';
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv
-('CHANNEL_ACCESS_TOKEN'));
-
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getnev(
-  'CHANNEL_SECRET')]);
-
-$signature = $_SERVER['HTTP_' . \LINE\LINEBot\Constant\HTTPHeader
-::LINE_SIGNATURE];
-
-$events = $bot->parseEventRequest(file_get_contents('php://input'),
-$signature);
-
+require_once __DIR__ . '/vendor/autoload.php';
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getnev('CHANNEL_SECRET')]);
+$signature = $_SERVER['HTTP_' . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
+$events = $bot->parseEventRequest(file_get_contents('php://input'),$signature);
 foreach ($events as $event){
   $bot->replyText($event->getReplyToken(), 'TextMessage');
 }
