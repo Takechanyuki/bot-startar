3cdf12b5c3cae0063157bc43f244c65ae7a87eba
Takechanyuki <yuki6023778@gmail.com>
2018-12-28 19:07:27 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 
+require_once __DIR__ . '/vendor/autoload.php';
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getnev('CHANNEL_SECRET')]);
 $signature = $_SERVER['HTTP_' . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
